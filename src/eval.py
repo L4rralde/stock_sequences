0cc7ff1ae848b59b1917ac47f2eb0365c2450c03
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-05-12 11:48:37 -0600

    Adding eval function

---
+++ src/eval.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+def eval(model: nn.Module, test_dataloader: object) -> float:
+    loss_fn = nn.MSELoss()
+    total_loss = 0.0
+    num_samples = 0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in test_dataloader:
+            y_hat = model(x)
+            loss = loss_fn(
+                torch.squeeze(y_hat),
+                torch.squeeze(y)
+            )
+
+            batch_size = x.size(0)
+            total_loss += loss.data.item() * batch_size
+            num_samples += batch_size
+    
+    total_loss /= num_samples
+    return total_loss
